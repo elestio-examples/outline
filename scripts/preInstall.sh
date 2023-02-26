da6f9551a83be93db8aa64eff4ffa352c87c0373
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-02-26 11:06:50 +0200

    fix

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -1,7 +1,10 @@
 #set env vars
 #set -o allexport; source .env; set +o allexport;
 
-apt install jq -y
+# apt install jq -y
 
-mkdir -p ./data
-chown -R 1000:1000 ./data
+# mkdir -p ./data
+# chown -R 1000:1000 ./data
+
+docker-compose run --rm outline yarn db:create --env=production-ssl-disabled
+docker-compose run --rm outline yarn db:migrate --env=production-ssl-disabled
