dfd7ce31ca161f1376af949e44d9daa774946c08
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-03-27 10:01:15 +0300

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -5,3 +5,5 @@ set -o allexport; source .env; set +o allexport;
 echo "Waiting for software to be ready ..."
 sleep 30s;
 
+    docker-compose exec ${PIPELINE_NAME}_wk-oidc-server_1 bash -c "make init"
+	docker-compose exec ${PIPELINE_NAME}_wk-oidc-server_1 bash -c "python manage.py loaddata oidc-server-outline-client"
