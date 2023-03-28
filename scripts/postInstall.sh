4bd63dac315acbc07b2ec6b2fc23df82854be07a
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-03-28 13:59:32 +0300

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -8,10 +8,12 @@ sleep 30s;
     docker-compose exec ${PIPELINE_NAME}_wk-oidc-server_1 bash -c "make init"
 	docker-compose exec ${PIPELINE_NAME}_wk-oidc-server_1 bash -c "python manage.py loaddata oidc-server-outline-client"
 
-echo "Restarting ..."
-
     docker-compose down;
-    docker-compose up;
+    docker-compose up -d;
+    echo "Restarting ..."
+    sleep 60s;
+
+
 
     docker exec -t ${PIPELINE_NAME}_wk-oidc-server_1 sh -c "python manage.py createsuperuser --noinput --username=admin --email=$ADMIN_EMAIL"
 	docker exec -t ${PIPELINE_NAME}_wk-oidc-server_1 sh -c "python manage.py shell <<EOF
@@ -21,8 +23,3 @@ echo "Restarting ..."
     user.set_password('$ADMIN_PASSWORD')
     user.save()
     EOF"
-
-echo "Restarting ..."
-
-	docker-compose down;
-	docker-compose up -d;
