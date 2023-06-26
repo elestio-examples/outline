f9d9de0ed913deb207d8c42e762176bebc16881b
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-06-26 14:08:31 +0300

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -6,8 +6,8 @@ echo "Waiting for software to be ready ..."
 
 sleep 60s;
 
-    docker exec ${PIPELINE_NAME}_wk-oidc-server_1 bash -c "make init"
-	docker exec ${PIPELINE_NAME}_wk-oidc-server_1 bash -c "python manage.py loaddata oidc-server-outline-client"
+    docker-compose exec -T wk-oidc-server bash -c "make init"
+	docker-compose exec -T wk-oidc-server bash -c "python manage.py loaddata oidc-server-outline-client"
 
 
 
