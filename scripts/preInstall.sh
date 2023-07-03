115512c7aa3b516278c81f0d23094df14619895c
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-07-03 13:23:39 +0300

    reorganization of the repository

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -7,7 +7,7 @@ chmod +x ./scripts/*.sh
 mkdir -p ./config/uc/fixtures
 chown -R 1000:1000 ./config/uc/fixtures
 
-if [ -e "./env.minio" ]; then
+if [ -e "./config/uc/fixtures/oidc-server-outline-client.json" ]; then
    exit 0;
 fi
 
@@ -42,102 +42,40 @@ MINIO_VERSION=RELEASE.2022-11-17T23-20-09Z
 MINIO_MC_VERSION=RELEASE.2022-11-17T21-20-39Z
 MINIO_ACCESS_KEY=${MINIO_ACCESS_KEY}
 MINIO_SECRET_KEY=${MINIO_SECRET_KEY}
-
-EOT
-
-cat << EOT >> ./env.minio
-
-MINIO_ACCESS_KEY=${MINIO_ACCESS_KEY}
-MINIO_SECRET_KEY=${MINIO_SECRET_KEY}
 MINIO_BROWSER=off
-
-EOT
-
-cat << EOT >> ./env.oidc
-
 OIDC_CLIENT_ID=050984
 OIDC_CLIENT_SECRET=${OIDC_CLIENT_SECRET}
 OIDC_AUTH_URI=$URL_ADDRESS/uc/oauth/authorize/
 OIDC_TOKEN_URI=http://wk-nginx/uc/oauth/token/
 OIDC_USERINFO_URI=http://wk-nginx/uc/oauth/userinfo/
-
 OIDC_USERNAME_CLAIM=preferred_username
-
 OIDC_DISPLAY_NAME=OpenID
-
-OIDC_SCOPES=openid profile email
-
-EOT
-
-cat << EOT >> ./env.oidc-server
-
+OIDC_SCOPES="openid profile email"
 DEBUG=0
 LANGUAGE_CODE=en-us
 TIME_ZONE=UTC
 FORCE_SCRIPT_NAME=/uc
 SECRET_KEY=${OUTLINE_SECRET_KEY}
-
-
-EOT
-
-cat << EOT >> ./env.outline
-
-# Copy this file to .env, remove this comment and change the keys. For development
-# with docker this should mostly work out of the box other than setting the Slack
-# keys (for auth) and the SECRET_KEY.
-#
-# Please use `openssl rand -hex 32` to create SECRET_KEY
-SECRET_KEY=${OUTLINE_SECRET_KEY}
 UTILS_SECRET=${OUTLINE_UTILS_SECRET}
-
-
-# Must point to the publicly accessible URL for the installation
 URL=$URL_ADDRESS
 PORT=3000
-
-# Optional. If using a Cloudfront distribution or similar the origin server
-# should be set to the same as URL.
 CDN_URL=$URL_ADDRESS
-
-# enforce (auto redirect to) https in production, (optional) default is true.
-# set to false if your SSL is terminated at a loadbalancer, for example
 FORCE_HTTPS=false
-
 ENABLE_UPDATES=true
-DEBUG=cache,presenters,events,emails,mailer,utils,multiplayer,server,services
-
-# Third party signin credentials (at least one is required)
-
-# To configure Google auth, you'll need to create an OAuth Client ID at
-# => https://console.cloud.google.com/apis/credentials
-#
-# When configuring the Client ID, add an Authorized redirect URI:
-# https://<your Outline URL>/auth/google.callback
 GOOGLE_CLIENT_ID=
 GOOGLE_CLIENT_SECRET=
-
-# Comma separated list of domains to be allowed (optional)
-# If not set, all Google apps domains are allowed by default
-GOOGLE_ALLOWED_DOMAINS=
-
-# Third party credentials (optional)
 SLACK_VERIFICATION_TOKEN=
 SLACK_MESSAGE_ACTIONS=false
 GOOGLE_ANALYTICS_ID=
 SENTRY_DSN=
-
-# AWS credentials (optional in development)
+GOOGLE_ALLOWED_DOMAINS=
 AWS_ACCESS_KEY_ID=${MINIO_ACCESS_KEY}
 AWS_SECRET_ACCESS_KEY=${MINIO_SECRET_KEY}
 AWS_REGION=xx-xxxx-x
 AWS_S3_UPLOAD_BUCKET_URL=$URL_ADDRESS
 AWS_S3_UPLOAD_MAX_SIZE=26214400
 AWS_S3_FORCE_PATH_STYLE=true
-# uploaded s3 objects permission level, default is private
-# set to "public-read" to allow public access
 AWS_S3_ACL=private
-
-# Emails configuration (optional)
 SMTP_HOST=${SMTP_HOST}
 SMTP_PORT=${SMTP_PORT}
 SMTP_USERNAME=${SMTP_USERNAME}
@@ -145,17 +83,10 @@ SMTP_PASSWORD=${SMTP_PASSWORD}
 SMTP_FROM_EMAIL=${SMTP_FROM_EMAIL}
 SMTP_REPLY_EMAIL=${SMTP_FROM_EMAIL}
 SMTP_SECURE=false
-
-# Custom logo that displays on the authentication screen, scaled to height: 60px
 # TEAM_LOGO=https://example.com/images/logo.png
-
-# See translate.getoutline.com for a list of available language codes and their
-# percentage translated.
 DEFAULT_LANGUAGE=en_US
 PGSSLMODE=disable
 ALLOWED_DOMAINS=
-
-
 
 EOT
 
